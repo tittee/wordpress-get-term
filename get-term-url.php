f3727e3ec6ebe7b6a00d5af86e82f58acb1bfd8f
Wittawat Kittiwarabud <tee.emilond@gmail.com>
2019-08-07 13:00:06 +0700

    Changes bug
    Changes bug. i wrong parameter in taxonomy

--- get-term-url.php
+++ get-term-url.php
@@ -1,7 +1,7 @@
 <?php
 
 // Get term by id (''term_id'') in Categories taxonomy.
-$term_slug = get_query_var( 'term' );
-$term_taxonomy = get_query_var( 'team_category' );
+$term_slug = get_query_var( 'TERM_SLUG' ); // EX : team_categoty, product_category, etc.
+$term_taxonomy = get_query_var( 'taxonomy' );
 $current_term = get_term_by( 'slug', $term_slug, $term_taxonomy );
 echo $current_term;
